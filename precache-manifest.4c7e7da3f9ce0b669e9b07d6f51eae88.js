3c5d9517cac06904f15e6c68d4e1bf0b7650d02f
JasonGit10 <jasonkev21@gmail.com>
2019-09-01 23:48:41 -0300

    Updates

---
+++ precache-manifest.4c7e7da3f9ce0b669e9b07d6f51eae88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6effc917dcdf08276c9bfbce1b0a8ace",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7718567b560450ff0bf4",
+    "url": "/monsters-rolodex/static/css/main.cfba0543.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monsters-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "7718567b560450ff0bf4",
+    "url": "/monsters-rolodex/static/js/main.23e9201a.chunk.js"
+  },
+  {
+    "revision": "c7b84e328d40a43989ce",
+    "url": "/monsters-rolodex/static/js/runtime~main.45a8f4a3.js"
+  }
+]);
